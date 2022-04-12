7bd008ab91bf9192cd7c0aa26dfb91e307e00d10
Yuvraj singh Dodiya <jarvuy7@gmail.com>
2022-04-12 12:26:14 +0530

    Gemlock

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-
     user = User.find_by(email: params[:email])
     # finds existing user, checks to see if user can be authenticated
     if user.present? && user.authenticate(params[:password])
