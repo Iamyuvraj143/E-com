3240d4c67b5f06052ced65a5b8d5aa6387277e37
Yuvraj Singh Dodiya <67416503+Iamyuvraj143@users.noreply.github.com>
2022-05-09 14:05:36 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   resources :shopping_cart, only: [:index]
   resources :cart_products
   resources :orders, only: %i( new edit create index show update)
-  resources :batch_orders, only: %i( new index )
   resources :cart_products, only: %i( new create destroy )
   resources :users, only: [:show] do
     resources :addresses, only: %i( new edit create update destroy )
