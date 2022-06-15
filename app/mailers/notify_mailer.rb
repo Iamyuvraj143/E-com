6ea6d790d0645eae48063161eba769d65398bf72
Yuvraj singh Dodiya <jarvuy7@gmail.com>
2022-06-15 17:53:54 +0530

    fdfd

--- app/mailers/notify_mailer.rb
+++ app/mailers/notify_mailer.rb
@@ -1,10 +1,10 @@
-class NotifyMailer < ApplicationMailer
-  default from: 'mymail@gmail.com'
+# class NotifyMailer < ApplicationMailer
+#   default from: 'mymail@gmail.com'
 
-  def notify_email
-    @notify_item = params[:notify_item]
-    @product = Product.find(params[:product_id])
-    @url = 'http://ecom.com/product_path'
-    mail(to: @notify_item.email, subject: "Notification from E-com #{@product.name} Update")
-  end
-end
+#   def notify_email
+#     @notify_item = params[:notify_item]
+#     @product = Product.find(params[:product_id])
+#     @url = 'http://ecom.com/product_path'
+#     mail(to: @notify_item.email, subject: "Notification from E-com #{@product.name} Update")
+#   end
+# end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
-class UserMailer < ApplicationMailer
-  default from: 'mymail@gmail.com'
+# class UserMailer < ApplicationMailer
+#   default from: 'mymail@gmail.com'
 
-  def welcome_email
-    @user = params[:user]
-    @url  = 'http://ecom.com/login'
-    mail(to: @user.email, subject: 'Welcome to My E-com Site')
-  end
-end
+#   def welcome_email
+#     @user = params[:user]
+#     @url  = 'http://ecom.com/login'
+#     mail(to: @user.email, subject: 'Welcome to My E-com Site')
+#   end
+# end
